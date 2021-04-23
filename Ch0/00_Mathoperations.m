ead8b9124d161f62cf9b824adaa29fe5fc5ac886
arash mehrzadi <48579846+arash-mehrzadi@users.noreply.github.com>
2021-04-23 23:25:10 +0430

    Add files via upload

---
+++ Ch0/00_Mathoperations.m
@@ -0,0 +1,13 @@
+%{
+---------------------------------------------
+This Program was Created by the MATLAB R2014b
+Project : Math Operations
+Author : Arash Mehrzadi https://arashmehrzadi.com
+Date : 23 April 2021
+---------------------------------------------
+%}
+fprintf('Run Mathoperations.m ...');
+X= input('\nPlease Enter X :');
+fprintf('\nfunction : X^3+3*X^2+6*X+6')
+Y= X^3+3*X^2+6*X+6;
+fprintf('\nResult : %d\n',Y);
